02e902068072417b7e8bca5a7a69d1c7b1aa7e41
anryabrahamyan <anryabrahamyan@mail.ru>
2025-10-17 15:50:40 +0000

    format files with fmt

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-    description = "The AWS region to deploy the resources in."
-    type        = string
-    default     = "us-east-1"    
+  description = "The AWS region to deploy the resources in."
+  type        = string
+  default     = "us-east-1"
 }
