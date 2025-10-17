9f08d770331c8a687dd23ec7794ec4560c3dd2ec
anryabrahamyan <anryabrahamyan@mail.ru>
2025-10-17 15:49:44 +0000

    add terraform lock file

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,14 @@
 terraform {
+    required_version = "1.13.3"
     required_providers {
         aws = {
         source  = "hashicorp/aws"
         version = "6.17.0"
         }
     }
+    #s3 backend not configured to save time during creation
+}
+
+provider "aws" {
+    region = var.aws_region #TODO move variables to tfvar   
 }
